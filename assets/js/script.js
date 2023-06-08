1a5d2282a4a400da59a81043e6794f7f2db76a71
Cyril-CRGB <>
2023-06-08 19:54:34 +0000

    Debug function -generateListOfProduct-, add text color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,48 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the button element
-    var generateButton = document.getElementById('generateButton');
-
-    // Add click event listener to the button
-    generateButton.addEventListener('click', generateListOfProduct);
-});
-
 /**
  * This function will add Price CHF and Price EUR of all table and update 
  * the table in index.html with the result.
  */
 function generateListOfProduct() {
     console.log('Button clicked');
+
     // Get references to the three tables
-    var table1 = document.getElementById('table1');
-    var table2 = document.getElementById('table2');
-    var table3 = document.getElementById('table3');
+    let table1 = document.getElementsByClassName('table10');
+    let table2 = document.getElementsByClassName('table200');
+    let table3 = document.getElementsByClassName('table210');
+    console.log(table1[0].children[1].textContent);
 
     // Get the tbody element of the new table
-    var newTableBody = document.getElementById('newTableBody');
+    let newTableBody = document.getElementById('newTableBody');
 
     // Clear any existing rows in the new table
-    /*newTableBody.innerHTML = '';*/
+    newTableBody.innerHTML = '';
+
 
     // Loop through each row of table1
-    for (var i = 1; i < table1.rows.length; i++) {
+    for (let i = 0; i < 6; i++) {
         // Create a new row for the new table
-        var newRow = document.createElement('tr');
+        let newRow = document.createElement('tr');
 
-        // Get the cells from table1
-        var table1Cells = table1.rows[i].cells;
-
-        // Create cells in the new row and set their values
-        for (var x = 0; x < 6; x++) {
-            var newCell = document.createElement('td');
-            newCell.textContent = table1Cells[x].textContent;
+        // Get the rows from table1
+        let table1Cells = table1[0].children[i];
+        console.log(table1Cells);
+        // Loop through each column of table1 and create cells in the new row and set their values
+        for (let x = 0; x < 6; x++) {
+            let newCell = document.createElement('td');
+            newCell.textContent = table1Cells.children[x].innerHTML;
             newRow.appendChild(newCell);
         }
-
+        console.log(table1Cells.children[6].innerHTML);
         // Calculate the sum of columns 7 and 8 from table1, table2, and table3
-        var sumColumn7 = parseFloat(table1Cells[6].textContent) + parseFloat(table2.rows[i].cells[6].textContent) + parseFloat(table3.rows[i].cells[6].textContent);
-        var sumColumn8 = parseFloat(table1Cells[7].textContent) + parseFloat(table2.rows[i].cells[7].textContent) + parseFloat(table3.rows[i].cells[7].textContent);
+        let sumColumn7 = parseFloat(table1Cells.children[6].innerHTML) + parseFloat(table2[0].children[i].children[6].innerHTML) + parseFloat(table3[0].children[i].children[6].innerHTML);
+        let sumColumn8 = parseFloat(table1Cells.children[7].innerHTML) + parseFloat(table2[0].children[i].children[7].innerHTML) + parseFloat(table3[0].children[i].children[7].innerHTML);
 
         // Create cells for the sum values and add them to the new row
-        var sumCell7 = document.createElement('td');
+        let sumCell7 = document.createElement('td');
         sumCell7.textContent = sumColumn7.toFixed(2);
         newRow.appendChild(sumCell7);
 
-        var sumCell8 = document.createElement('td');
+        let sumCell8 = document.createElement('td');
         sumCell8.textContent = sumColumn8.toFixed(2);
         newRow.appendChild(sumCell8);
 
@@ -56,3 +50,10 @@ function generateListOfProduct() {
         newTableBody.appendChild(newRow);
     }
 }
+
+
+// Get the button element
+let generateButton = document.getElementById('generateButton');
+
+// Add click event listener to the button
+generateButton.addEventListener('click', generateListOfProduct);
